3d7302cabd06e183209135daeeefd0c0636753ed
Eric Carmichael <eric@ckcollab.com>
2021-10-22 15:46:57 -0700

    test softdelete queryset

--- tests/integration/test_soft_delete.py
+++ tests/integration/test_soft_delete.py
@@ -1,3 +1,4 @@
+import pytest
 from django.test import TestCase
 
 from testapp.models import AModel
@@ -7,6 +8,12 @@ class TestSoftDelete(TestCase):
 
     def test_soft_delete_model_doesnt_really_delete(self):
         instance = AModel.objects.create()
+
+        # normal model deletion..
         instance.delete()
         assert not AModel.objects.filter(pk=instance.pk).exists()
         assert AModel.all_objects.filter(pk=instance.pk).exists()
+
+        # bulk deletion from queryset also protected...
+        with pytest.raises(NotImplementedError, match="Delete is allowed on SoftDeleteQuerySet."):
+            AModel.objects.all().delete()

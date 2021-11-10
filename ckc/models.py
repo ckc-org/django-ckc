53f0aff8ce89a7bc838011edca86f65cc2d8878d
Bailey Lind-Trefts <gibson.bailey@live.com>
2021-11-10 13:23:37 -0800

    Queryset delete (#27)
    * Test queryset deletion.
    
    * Set deleted field to True on Queryset.delete rather than raising NotImplementedError.
    
    * f8

--- ckc/models.py
+++ ckc/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SoftDeleteQuerySet(models.QuerySet):
     def delete(self):
-        raise NotImplementedError("Delete is allowed on SoftDeleteQuerySet.")
+        return self.update(deleted=True)
 
 
 class SoftDeleteModelManager(models.Manager):
--- tests/integration/test_soft_delete.py
+++ tests/integration/test_soft_delete.py
@@ -1,4 +1,3 @@
-import pytest
 from django.test import TestCase
 
 from testapp.models import AModel
@@ -14,6 +13,10 @@ class TestSoftDelete(TestCase):
         assert not AModel.objects.filter(pk=instance.pk).exists()
         assert AModel.all_objects.filter(pk=instance.pk).exists()
 
-        # bulk deletion from queryset also protected...
-        with pytest.raises(NotImplementedError, match="Delete is allowed on SoftDeleteQuerySet."):
-            AModel.objects.all().delete()
+    def test_soft_delete_queryset_doesnt_really_delete(self):
+        instance = AModel.objects.create()
+
+        # queryset deletion..
+        AModel.objects.all().delete()
+        assert not AModel.objects.filter(pk=instance.pk).exists()
+        assert AModel.all_objects.filter(pk=instance.pk).exists()

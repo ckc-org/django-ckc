5a05d0ddfb60819bf70806cbf7919926b69fe0aa
Eric Carmichael <eric@ckcollab.com>
2022-01-05 15:18:24 -0800

    to_representation as PK where needed

--- ckc/fields.py
+++ ckc/fields.py
@@ -17,3 +17,25 @@ class PrimaryKeyWriteSerializerReadField(serializers.PrimaryKeyRelatedField):
 
     def to_representation(self, value):
         return self.read_serializer(value, context=self.context).data
+
+    def get_choices(self, cutoff=None):
+        """Copied from base get_choices, replacing to_representation directly with PK, 
+        since our to_representation returns a dict."""
+        queryset = self.get_queryset()
+        if queryset is None:
+            # Ensure that field.choices returns something sensible
+            # even when accessed with a read-only field.
+            return {}
+
+        if cutoff is not None:
+            queryset = queryset[:cutoff]
+
+        return OrderedDict([
+            (
+                # Instead of to_representation, just snag pk!
+                # self.to_representation(item),
+                item.pk,
+                self.display_value(item)
+            )
+            for item in queryset
+        ])

82fe6630898a86a6e0b1f2d8b2fed1175bdb848b
Eric Carmichael <eric@ckcollab.com>
2022-10-31 09:43:16 -0700

    Add json model snapshots + tests (#32)
    * add json model snapshots + tests
    
    * remove python 3.11

--- ckc/models.py
+++ ckc/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class SoftDeleteQuerySet(models.QuerySet):
@@ -23,3 +24,35 @@ class SoftDeletableModel(models.Model):
     def delete(self, *args, **kwargs):
         self.deleted = True
         self.save()
+
+
+class JsonSnapshotModel(models.Model):
+    """This mixin is meant to be inherited by a model class. It creates a snapshot field for the class that it is a part of.
+     This field is used to solidify data at a given point in time.
+
+    The create_json_snapshot() method must be overridden in the class inheriting this mixin. Inside this method you will build
+    a custom JSON object of your model state. Include the fields you wish to be solidified.
+
+    Lastly, call take_snapshot() at the point in your code you want data to be saved. The time and date this occurs will
+    also be saved in a separate field called snapshot_date.
+    """
+    snapshot = models.JSONField(null=True, blank=True, default=dict)
+    snapshot_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def _create_json_snapshot(self) -> dict:
+        """Override this method to take a "snapshot" of the relevant data on this model"""
+        raise NotImplementedError
+
+    def take_snapshot(self, force=False):
+        if not force:
+            assert not self.snapshot, "Can not override an existing snapshot instance."
+
+        self.snapshot = self._create_json_snapshot()
+
+        # TODO: Do we want to test these edge cases?
+        # assert self.snapshot is not None, ""
+
+        self.snapshot_date = now()
--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -2,20 +2,29 @@ from django.contrib.auth import get_user_model
 from django.contrib.gis.db.models import PointField
 from django.db import models
 
-from ckc.models import SoftDeletableModel
+from ckc.models import SoftDeletableModel, JsonSnapshotModel
 
 
 User = get_user_model()
 
 
+# ----------------------------------------------------------------------------
+# Testing soft deletable model
+# ----------------------------------------------------------------------------
 class AModel(SoftDeletableModel):
     title = models.CharField(max_length=255, default="I'm a test!")
 
 
+# ----------------------------------------------------------------------------
+# PrimaryKeyWriteSerializerReadField related model
+# ----------------------------------------------------------------------------
 class BModel(models.Model):
     a = models.ForeignKey(AModel, on_delete=models.CASCADE)
 
 
+# ----------------------------------------------------------------------------
+# DefaultCreatedByMixin models
+# ----------------------------------------------------------------------------
 class ModelWithACreator(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -24,5 +33,25 @@ class ModelWithADifferentNamedCreator(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
+# ----------------------------------------------------------------------------
+# For testing geo points in factories
+# ----------------------------------------------------------------------------
 class Location(models.Model):
     geo_point = PointField()
+
+
+# ----------------------------------------------------------------------------
+# For testing JSON snapshots
+# ----------------------------------------------------------------------------
+class SnapshottedModel(JsonSnapshotModel, models.Model):
+
+    def _create_json_snapshot(self) -> dict:
+        return {
+            "test": "snapshot"
+        }
+
+
+class SnapshottedModelMissingOverride(JsonSnapshotModel, models.Model):
+    # No _create_json_snapshot here! This is for testing purposes, to confirm we raise
+    # an assertion when this method is missing
+    pass

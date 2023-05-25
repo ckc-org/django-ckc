ef423fa95784aacfbd7447b216c55468999bfa10
Eric Carmichael <eric@ckcollab.com>
2023-05-24 19:40:06 -0700

    add example for PrimaryKeyWriteSerializerReadField

--- ckc/fields.py
+++ ckc/fields.py
@@ -4,6 +4,15 @@ from rest_framework import serializers
 
 
 class PrimaryKeyWriteSerializerReadField(serializers.PrimaryKeyRelatedField):
+    """Helper for writing m2m/lists as PKs, while reading via a serializer.
+    
+    For example:
+    
+        address = PrimaryKeyWriteSerializerReadField(
+           queryset=ShippingAddress.objects.all(),
+           read_serializer=ShippingAddressSerializer
+        )
+    """
     def __init__(self, *args, **kwargs):
         read_serializer = kwargs.pop('read_serializer', None)
         assert read_serializer is not None, (
